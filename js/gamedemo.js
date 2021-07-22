02b1a18188f915b2d36c864ff87911067eb5d17f
yoav2001 <yoavelkana@gmail.com>
2021-07-22 16:52:39 +0300

    change for ===, and syntach example

--- js/gamedemo.js
+++ js/gamedemo.js
@@ -1,56 +1,43 @@
-function calculateResult(nameleft, codeleft, nameright, coderight) {
-
-
-    const stone = { nameFight: "stone", code: 1 };
-    const paper = { nameFight: "paper", code: 2 };
-    const scissors = { nameFight: "scissors", code: 3 };
-
-
-    const player_left = { name: nameleft, choosetiCode: codeleft, iswinner: false };
-    const player_right = { name: nameright, chooseCode: coderight, iswinner: false };
-
-    const player_teko = { name: "teko", chooseCode: null };
-
-
-    if (gameparams.player1win) return {
-        winner: 1
-    }
-
-}
-
-
-
-
 function game(nameleft, codeleft, nameright, coderight) {
     const stone = { nameFight: "stone", code: 1 };
     const paper = { nameFight: "paper", code: 2 };
     const scissors = { nameFight: "scissors", code: 3 };
+
+    const x = {
+        a: 1,
+        b: 2
+    }
+
+    const y = {
+        ...x,
+        b: 3
+    }
 
 
     const player_left = { name: nameleft, chooseCode: codeleft, iswinner: false };
     const player_right = { name: nameright, chooseCode: coderight, iswinner: false };
 
     const player_teko = { name: "teko", chooseCode: null };
-
+    console.log(player_teko)
     const result = { winner: player_teko, loser: player_teko, sideWin: null, is_teko: false };
 
-    if (codeleft == coderight) {
+    if (codeleft === coderight) {
         result.is_teko = true;
         result.winner.chooseCode = codeleft; //זה לא משנה יכלתי להשוות לימיני
+        console.log(player_teko)
+
     }
-    if (codeleft == 1 && coderight == 2) {
-        result.winner = player_right;
-        result.loser = player_left;
-        result.sideWin = "right";
+    if (codeleft === 1 && coderight === 2) {
+        return {}
     }
 
-    if (codeleft == 1 && coderight == 3) {
+    if (codeleft === 1 && coderight === 3) {
         result.winner = player_left;
         result.loser = player_right;
         result.sideWin = "left";
     }
 
-    if (codeleft == 2 && coderight == 1) {
+    if (codeleft === 2 && coderight === 1) {
 
         result.winner = player_left;
         result.loser = player_right;
@@ -58,14 +45,14 @@ function game(nameleft, codeleft, nameright, coderight) {
 
     }
 
-    if (codeleft == 2 && coderight == 3) {
+    if (codeleft === 2 && coderight === 3) {
 
         result.winner = player_right;
         result.loser = player_left;
         result.sideWin = "right";
     }
 
-    if (codeleft == 3 && coderight == 1) {
+    if (codeleft === 3 && coderight === 1) {
 
 
         result.winner = player_right;
@@ -74,7 +61,7 @@ function game(nameleft, codeleft, nameright, coderight) {
 
     }
 
-    if (codeleft == 3 && coderight == 2) {
+    if (codeleft === 3 && coderight === 2) {
 
         result.winner = player_left;
         result.loser = player_right;
@@ -98,4 +85,24 @@ function game(nameleft, codeleft, nameright, coderight) {
     return result;
 
 
+}
+
+function is_key_code_correct(key_value, currElement, index) {
+    if (key_value == currElement) {
+        if (index < 3 && game_state_parameters.select_left_keycode === -1) {
+            game_state_parameters.select_left_keycode = index + 1; //INDEX 0,1,2=בגלל זה מוסיף אחד
+            game_state_parameters.check_key_bord_fight = true;
+            game_state_parameters.count_click_key_bord++;
+        } else if (index < 3 && game_state_parameters.select_left_keycode != -1) {
+            game_state_parameters.check_key_bord_fight = false;
+        }
+        if (index > 2 && game_state_parameters.select_right_keycode === -1) {
+            game_state_parameters.select_right_keycode = index - 2; //index --3,4,5=-2=1,2,3
+            game_state_parameters.check_key_bord_fight = true;
+            game_state_parameters.count_click_key_bord++;
+        } else if (index > 2 && game_state_parameters.select_right_keycode != -1) {
+            game_state_parameters.check_key_bord_fight = false;
+        }
+
+    }
 }
